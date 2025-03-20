84c283ddb35d33d7c0880e89c4ba1f8bbc6409bc
srivastav2005 <srivastavdileep2005@gmail.com>
2025-03-20 13:26:24 +0530

    Progress day 2
    Learnt IDLE & basic features

---
+++ working on scrip mode.py
@@ -0,0 +1,13 @@
+# Comments
+
+
+# to dislplay output
+print("We are working on the scrip mode")
+
+print("we are learing py"* 4)
+
+#calculation
+price=90
+pocket_money = 100
+
+print("After buying Apples,balance now :",(pocket_money - price ))
